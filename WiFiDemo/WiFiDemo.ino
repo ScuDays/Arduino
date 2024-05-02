df8719168db71b3030e6adac038f1a27fb84cfb3
ScuDays <dayss1224@gmail.com>
2024-05-02 21:30:46 +0800

    Wifi_Weather_Demo

---
+++ WiFiDemo/WiFiDemo.ino
@@ -0,0 +1,48 @@
+#include "WiFi.h"
+#include "ArduinoJson.h"
+#include "HTTPClient.h"
+static DynamicJsonDocument doc(1024);
+HTTPClient http;
+
+const char* ssid = "AE86";
+const char* password = "Ydhs857878318";
+const char* apiServer = "https://api.map.baidu.com/weather/v1/?district_id=510116&data_type=all&ak=lf4xE60gV2B9SwMBexugoZFGrKbwxEij";
+
+static String weathertext;
+static int temp;
+
+void getWeather(){
+  http.begin(apiServer);
+  int httpCode = http.GET();
+  delay(500);
+  if(httpCode == HTTP_CODE_OK){
+    String responseData = http.getString();
+    deserializeJson(doc, responseData);
+
+    JsonObject result = doc["result"];
+    JsonObject now= result["now"];
+
+    weathertext = now["text"].as<String>();
+    temp = now["temp"].as<int>();
+    Serial.println(weathertext);
+    Serial.println(temp);
+    Serial.println(responseData);
+  }
+  http.end();
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("开始连接WiFi！");
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(100);
+  }
+  Serial.println(WiFi.localIP());
+  getWeather();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
